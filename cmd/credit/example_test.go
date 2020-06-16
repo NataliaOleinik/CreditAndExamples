b3024046439c1f0c69b25f742829021ae208d115
Natalia Oleinik <olnataly81@gmail.com>
2020-06-16 15:01:14 +0700

    example added

--- cmd/credit/example_test.go
+++ cmd/credit/example_test.go
@@ -1,14 +1,14 @@
-package credit_test
+package credit_test // взяли пакет credit, добавили _test
+
 import (
-	"fmt"
-	_ "fmt"
 	"github.com/NataliaOleinik/CreditAndExamples/pkg/credit"
+	"fmt"
 )
 
-func ExampleCalculate() {
+func ExampleCalculate() { // имя функции - Example + имя проверяемой функции
 	fmt.Println(credit.Calculate(1_000_000_00, 36, 20))
 	fmt.Println(credit.Calculate(10_000_00, 36, 20))
 	// Output:
 	// 3718400 33862300 133862300
 	// 37184 338623 1338623
- }
+}
