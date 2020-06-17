733fd9411bd9a0deacce6165faef961fd8fb44d0
Natalia Oleinik <olnataly81@gmail.com>
2020-06-18 00:20:01 +0700

    test edited

--- pkg/credit/example_test.go
+++ pkg/credit/example_test.go
@@ -1,13 +1,16 @@
 package credit_test
 
-import (
+import
+(
 	"fmt"
 	_ "fmt"
+	"github.com/NataliaOleinik/CreditAndExamples/pkg/credit"
 )
 
+
 func ExampleCalculate() {
-	fmt.Println(Calculate(1_000_000_00, 36, 20))
-	fmt.Println(Calculate(10_000_00, 36, 20))
+	fmt.Println(credit.Calculate(1_000_000_00, 36, 20))
+	fmt.Println(credit.Calculate(10_000_00, 36, 20))
 	// Output:
 	// 3718400 33862300 133862300
 	// 37184 338623 1338623
